bb5e6c0fd275198a08ae1836ee12e4463f308c03
Kamil <kgruca7747@gmail.com>
2024-11-13 16:26:42 +0100

    Dodanie kolejnych metod

--- Scalanie.cpp
+++ Scalanie.cpp
@@ -6,7 +6,52 @@ void MergeSort::Scalanie_Dzielenie(std::vector<int>& tab, int lewo, int prawo) {
 		int srodek = lewo + (prawo - lewo) / 2;
 		Scalanie_Dzielenie(tab, lewo, prawo);
 		Scalanie_Dzielenie(tab, srodek + 1, prawo);
+		Scalanie_Scalanie(tab, lewo, srodek, prawo);
 	}
 }
 
+void MergeSort::Scalanie_Scalanie(std::vector<int>& tab, int lewo, int srodek, int prawo) {
+	
+	int t1 = srodek - lewo + 1;
+	int t2 = prawo - srodek;
+	int i = 0;
+	int j = 0;
+	int k = lewo;
+
+	std::vector<int> lewatab(t1);
+	std::vector<int> prawatab(t2);
+
+	for (int i = 0; i < t1; i++)
+		lewatab[i] = tab[lewo + i];
+	for (int i = 0; i < t2; i++)
+		prawatab[i] = tab[srodek + 1 + i];
+
+	while (i < t1 && j < t2) {
+		if (lewatab[i] <= prawatab[j]) {
+			tab[k] = lewatab[i];
+			i++;
+		}
+		else {
+			tab[k] = prawatab[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < t1) {
+		tab[k] = lewatab[i];
+		i++;
+		k++;
+	}
+
+	while (j < t2) {
+		tab[k] = prawatab[j];
+		j++;
+		k++;
+	}
+}
+
+void MergeSort::Scalanie_Sortowanie(std::vector<int>& tab) {
+
+}
 
--- Scalanie.h
+++ Scalanie.h
@@ -7,5 +7,5 @@ class MergeSort {
 public:
 	void Scalanie_Sortowanie(std::vector<int>& tab);
 	void Scalanie_Dzielenie(std::vector<int>& tab, int lewo, int prawo);
-	void Scalanie_Scalanie(std::vector<int>& tab, int lewo, int srodek, int prawo)
+	void Scalanie_Scalanie(std::vector<int>& tab, int lewo, int srodek, int prawo);
 };
