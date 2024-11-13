32547a640faa37b8f664b88a3424e78eef798de2
Kamil <kgruca7747@gmail.com>
2024-11-13 16:32:11 +0100

    Dodanie metody

--- Scalanie.cpp
+++ Scalanie.cpp
@@ -4,8 +4,10 @@ void MergeSort::Scalanie_Dzielenie(std::vector<int>& tab, int lewo, int prawo) {
 
 	if (lewo < prawo) {
 		int srodek = lewo + (prawo - lewo) / 2;
+
 		Scalanie_Dzielenie(tab, lewo, prawo);
 		Scalanie_Dzielenie(tab, srodek + 1, prawo);
+
 		Scalanie_Scalanie(tab, lewo, srodek, prawo);
 	}
 }
