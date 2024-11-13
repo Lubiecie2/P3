23f499a04f47067bd3ac1e7de38577b53ab8872c
Kamil <kgruca7747@gmail.com>
2024-11-13 16:18:00 +0100

    Dodanie metod

--- Scalanie.cpp
+++ Scalanie.cpp
@@ -1,1 +1,12 @@
 #include "Scalanie.h"
+
+void MergeSort::Scalanie_Dzielenie(std::vector<int>& tab, int lewo, int prawo) {
+
+	if (lewo < prawo) {
+		int srodek = lewo + (prawo - lewo) / 2;
+		Scalanie_Dzielenie(tab, lewo, prawo);
+		Scalanie_Dzielenie(tab, srodek + 1, prawo);
+	}
+}
+
+
