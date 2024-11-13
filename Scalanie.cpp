0613c0dd688f9d5f1ce564ffbe29f8f9aa5fe64e
Kamil <kgruca7747@gmail.com>
2024-11-13 16:07:12 +0100

    Dodanie plikow

---
+++ Scalanie.cpp
@@ -0,0 +1,1 @@
+#include "Scalanie.h"
--- Scalanie.h
+++ Scalanie.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include <vector>
+
+class MergeSort {
+	
+public:
+	void Scalanie_Sortowanie(std::vector<int>& tab);
+	void Scalanie_Dzielenie(std::vector<int>& tab, int lewo, int prawo);
+	void Scalanie_Scalanie(std::vector<int>& tab, int lewo, int srodek, int prawo)
+};
