d43d1924fa3b81af4a081e3ad0e1fae97813e99f
Kamil <kgruca7747@gmail.com>
2024-11-17 16:32:24 +0100

    commit

--- Scalanie.cpp
+++ Scalanie.cpp
@@ -3,52 +3,52 @@
 void MergeSort::Scalanie_Dzielenie(std::vector<int>& tab, int lewo, int prawo) {  // <-- Funkcja jest odpowiedzialana za rekursywne dzielenie tablicy 
 
 	if (lewo < prawo) {									
-		int srodek = lewo + (prawo - lewo) / 2;		// <-- Tablica jest dzielona na dwie czêœci 
+		int srodek = lewo + (prawo - lewo) / 2;			// <-- Tablica jest dzielona na dwie czêœci 
 		
 		Scalanie_Dzielenie(tab, lewo, srodek);		    // <-- Rekursywnie jest dzielona lewa czêœæ tablicy
 
 		Scalanie_Dzielenie(tab, srodek + 1, prawo);		// <-- Rekursywnie jest dzielona prawa czêœæ tablicy 
 
-		Scalanie_Scalanie(tab, lewo, srodek, prawo);	// <-- Wywo³ywana jest funkcja która dzieli dwie posortowane czêœci 
+		Scalanie_Scalanie(tab, lewo, srodek, prawo);	// <-- Wywo³ywana jest funkcja która dzieli dwie posortowane czêœæ 
 	}
 }
 
 void MergeSort::Scalanie_Scalanie(std::vector<int>& tab, int lewo, int srodek, int prawo) {
 	
-	int t1 = srodek - lewo + 1;
-	int t2 = prawo - srodek;
-	int i = 0;
-	int j = 0;
-	int k = lewo;
+	int t1 = srodek - lewo + 1;  // <-- Rozmiar lewej podtablicy
+	int t2 = prawo - srodek;	 // <-- Rozmiar prawej podtablicy
+	int i = 0;		// <-- indeks dla lewej podtablicy
+	int j = 0;		// <-- indeks dla prawej podtablicy
+	int k = lewo;	// <-- indeks dla g³ównej tablicy
 
-	std::vector<int> lewatab(t1);
-	std::vector<int> prawatab(t2);
+	std::vector<int> lewatab(t1);	// <-- Tworzenie tymczasowej tablicy która zawiera elementy lewa do œrodka 
+	std::vector<int> prawatab(t2);	// <-- Tworzenie tymczasowej tablicy która zawiera elemetny od œrodka do prawa 
 
-	for (int i = 0; i < t1; i++)
+	for (int i = 0; i < t1; i++)			// <-- Kopiujemy dane do tymczasowej lewej tablicy
 		lewatab[i] = tab[lewo + i];
-	for (int i = 0; i < t2; i++)
+	for (int i = 0; i < t2; i++)			// <-- Kopiujemy dane do tymczasowej prawej tablicy
 		prawatab[i] = tab[srodek + 1 + i];
 
-	while (i < t1 && j < t2) {
-		if (lewatab[i] <= prawatab[j]) {
-			tab[k] = lewatab[i];
+	while (i < t1 && j < t2) {				// <-- Pêtla porównujemy elementy z obu tablic i umieszcza mniejsze elementy do g³ównej tablicy
+		if (lewatab[i] <= prawatab[j]) {	// <-- Jeœli elementy z lewej tab s¹ mniejsze lub równe ni¿ te z prawej tab wstawiamy 
+			tab[k] = lewatab[i];			//     element z lewej tablicy do g³ównej tablicy
 			i++;
 		}
-		else {
+		else {								// <-- Jeœli elementy z prawej tablicy s¹ mniejsze, wstawiamy element z prawej tablicy 
 			tab[k] = prawatab[j];
 			j++;
 		}
 		k++;
 	}
 
-	while (i < t1) {
-		tab[k] = lewatab[i];
+	while (i < t1) {			
+		tab[k] = lewatab[i];	// <-- Kopiowanie pozosta³ych elementów z lewej tablicy 
 		i++;
 		k++;
 	}
 
 	while (j < t2) {
-		tab[k] = prawatab[j];
+		tab[k] = prawatab[j];	// <-- Kopiowanie pozosta³ych elementów z prawej tablicy 
 		j++;
 		k++;
 	}
