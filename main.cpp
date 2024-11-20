29adc7895cd8c7a2b58f5cc427862acf7d925bb5
Kamil <kgruca7747@gmail.com>
2024-11-20 17:20:08 +0100

    zmiany

--- main.cpp
+++ main.cpp
@@ -5,10 +5,9 @@
 
 int main()
 {
-    MergeSort sortowanie;
-
-    std::cout << "Sortowanie Tablicy: " << std::endl;
+    std::cout << "Przykladowe sortowania tablic: " << std::endl;
     std::cout << std::endl;
+    MergeSort sortowanie;
 
     std::cout << "Sortowanie tablicy w odwrotnej kolejnosci: ";
     std::cout << std::endl;
@@ -25,6 +24,47 @@ int main()
 
     std::cout << "Po sortowaniu: ";
     for (int i : tab) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Sortowanie tablicy w losowej kolejnosci: ";
+    std::cout << std::endl;
+
+    std::vector<int> tab1 = { 4, 2, 1, 8, 6, 3, 9, 5, 7};
+
+    std::cout << "Przed sortowaniem: ";
+    for (int i : tab1) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    sortowanie.Scalanie_Sortowanie(tab1);
+
+    std::cout << "Po sortowaniu: ";
+    for (int i : tab1) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Sortowanie tablicy w z liczbami dodatnimi i ujemnymi: ";
+    std::cout << std::endl;
+
+    std::vector<int> tab2 = { -4, 2, -1, 8, -6, 3, -9, -5, 7 };
+
+    std::cout << "Przed sortowaniem: ";
+    for (int i : tab2) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    sortowanie.Scalanie_Sortowanie(tab2);
+
+    std::cout << "Po sortowaniu: ";
+    for (int i : tab2) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
